d9c4902b333bb69df92aea1c82e6b048bb103166
akilkumar28 <akil.psn@gmail.com>
2017-06-13 20:26:24 +0530

    small minor changes

--- Secret Agents/HomeVC.swift
+++ Secret Agents/HomeVC.swift
@@ -7,17 +7,62 @@
 //
 
 import UIKit
+import Firebase
 
-class HomeVC: UIViewController {
+class HomeVC: UIViewController,UITableViewDataSource,UITableViewDelegate {
     
     
+    @IBOutlet weak var myTableView: UITableView!
     
+    
+    var missions = [Mission]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.navigationController?.navigationBar.isHidden = false
+        
+        databaseRef.child("Users").child(Auth.auth().currentUser!.uid).child("missions").observe(.childAdded) { (snapshot:DataSnapshot) in
+            let mission = Mission()
+            if let stages = snapshot.value as? Dictionary<String,Any> {
+                if let detail = stages["details"] as? String {
+                    mission.detail = detail
+                }
+                if let from = stages["from"] as? String {
+                    mission.from = from
+                }
+                if let imageUrl = stages["imageUrl"] as? String {
+                    mission.imageUrl = imageUrl
+                }
+                if let uniqueName = stages["uniqueName"] as? String {
+                    mission.uniqueName = uniqueName
+                }
+                mission.key = snapshot.key
+                
+                self.missions.append(mission)
+                self.myTableView.reloadData()
+            }
+        }
+        
+        
+        
+        
 
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.missions.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = myTableView.dequeueReusableCell(withIdentifier: "Cell")
+        
+        cell?.textLabel?.text  = missions[indexPath.row].uniqueName
+        return cell!
     }
 
    
